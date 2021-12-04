7a37f4e0003d0e2a54dcc1794b1c5d4fbb24c4df
Tobias Kaiser <tobi.kaiser@fau.de>
2021-12-04 16:52:31 +0100

    4 done

--- 2021/3.py
+++ 2021/3.py
@@ -35,7 +35,47 @@ def get_life_support_rating():
     for i in range(len(data)):
         data[i] = data[i][:-1]
 
-    return 0
+    oxygen_generator_rating = data.copy()
+    co2_scrubber_rating = data.copy()
+
+    for i in range(len(data[0])):
+        if len(oxygen_generator_rating) == 1 and len(co2_scrubber_rating) == 1:
+            break
+
+        if len(oxygen_generator_rating) > 1:
+            balance = 0
+            for string in oxygen_generator_rating:
+                if string[i] == '1':
+                    balance += 1
+                else:
+                    balance -= 1
+            if balance >= 0:
+                oxygen_bit = '1'
+            else:
+                oxygen_bit = '0'
+
+        if len(co2_scrubber_rating) > 1:
+            balance = 0
+            for string in co2_scrubber_rating:
+                if string[i] == '1':
+                    balance += 1
+                else:
+                    balance -= 1
+            if balance >= 0:
+                co2_bit = '0'
+            else:
+                co2_bit = '1'
+
+            for string in list(oxygen_generator_rating):
+                if string[i] != oxygen_bit:
+                    oxygen_generator_rating.remove(string)
+
+            for string in list(co2_scrubber_rating):
+                if string[i] != co2_bit:
+                    co2_scrubber_rating.remove(string)
+
+    return [int("".join(oxygen_generator_rating[0]), 2), int("".join(co2_scrubber_rating[0]), 2)]
+
 
 def main():
     res = get_power_consumption()
---
+++ 2021/4.py
@@ -0,0 +1,68 @@
+def get_winning_score():
+    with open("4_input.txt", "r") as file:
+        data = file.readlines()
+
+    numbers = [int(x) for x in data[0].split(",")]
+    boards = []
+
+    counter = 0
+    for line in data[2:]:
+        if counter % 6 == 5:
+            counter += 1
+            continue
+        elif counter % 6 == 0:
+            boards.append([])
+        boards[counter // 6].extend([int(x) for x in line.split()])
+        counter += 1
+
+    for number in numbers:
+        for board in boards:
+            if number in board:
+                board[board.index(number)] = -1
+                for i in range(5):
+                    if board[i * 5:i * 5 + 5] == [-1] * 5 or board[i:-1:5] == [-1] * 5:
+                        # bingo
+                        return [sum([x if x != -1 else 0 for x in board]), number]
+    return [0, 0]
+
+
+def get_loosing_score():
+    with open("4_input.txt", "r") as file:
+        data = file.readlines()
+
+    numbers = [int(x) for x in data[0].split(",")]
+    boards = []
+
+    counter = 0
+    for line in data[2:]:
+        if counter % 6 == 5:
+            counter += 1
+            continue
+        elif counter % 6 == 0:
+            boards.append([])
+        boards[counter // 6].extend([int(x) for x in line.split()])
+        counter += 1
+
+    for number in numbers:
+        for board in list(boards):
+            if number in board:
+                board[board.index(number)] = -1
+                for i in range(5):
+                    if board[i * 5:i * 5 + 5] == [-1] * 5 or board[i:-1:5] == [-1] * 5:
+                        # bingo
+                        boards.remove(board)
+                        res = [sum([x if x != -1 else 0 for x in board]), number]
+                        break
+    return res
+
+
+def main():
+    res = get_winning_score()
+    print("{} * {} = {}".format(res[0], res[1], res[0] * res[1]))
+
+    res = get_loosing_score()
+    print("{} * {} = {}".format(res[0], res[1], res[0] * res[1]))
+
+
+if __name__ == '__main__':
+    main()
