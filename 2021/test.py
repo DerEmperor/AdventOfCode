d21fc93336baf846e2bc6e75f5a4ff197336fb6f
Tobias Kaiser <tobi.kaiser@fau.de>
2021-12-08 15:49:02 +0100

    8 done

--- 2021/test.py
+++ 2021/test.py
@@ -1,2 +1,19 @@
-print(list(range(0, 5+1)))
-print(list(range(5, 0-1, -1)))
+import itertools
+import time
+
+
+def main():
+    print(sum(int(''.join('53460_1_7_92__8'
+                          [(len(o) * len(o & (l := {len(s): set(s) for s
+                                                    in x.split()})[4]) + len(o & l[2])) % 16] for
+                          o in map(set, y.split()))) for
+              x, y in [x.split('|') for x in open('8_input.txt')]))
+
+
+if __name__ == '__main__':
+    startTime = time.time()
+
+    main()
+
+    executionTime = (time.time() - startTime)
+    print('Execution time: ' + str(round(executionTime * 1000, 6)) + ' ms')
