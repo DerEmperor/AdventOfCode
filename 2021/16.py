13545ce9a93a62981ffe7c031320f50308b6b7ab
Tobias Kaiser <tobi.kaiser@fau.de>
2021-12-16 14:59:41 +0100

    16 done

---
+++ 2021/16.py
@@ -0,0 +1,152 @@
+import time
+
+hex2bin = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111'
+}
+
+
+def main():
+    with open('16_input.txt', 'r') as file:
+        input_ = file.readlines()
+    data = ''.join([hex2bin[x] for x in input_[0][:-1]])
+
+    print(input_[0][:-1])
+    print(data)
+
+    version, number, end = evaluate_data(data)
+
+    print(version)
+    print(number)
+
+
+def evaluate_data(data):
+    version = int(data[:3], 2)
+    type_id = int(data[3:6], 2)
+
+    if type_id == 4:
+        # literal value
+
+        number, end = evaluate_number(data[6:])
+        end += 7
+        return version, number, end
+
+    else:
+        # operator
+        if data[6] == '0':
+            # 7 + 15 = 22
+            length_packages = int(data[7:22], 2)
+            sub_packets = data[22:22 + length_packages]
+
+            versions, numbers = evaluate_sub_packets_length(sub_packets)
+            end = 22 + length_packages
+
+        else:
+            number_sub_packets = int(data[7:7 + 11], 2)
+
+            versions, numbers, end = evaluate_sub_packets_number(data[18:], number_sub_packets)
+            end += 18
+
+    version += sum(versions)
+    number = apply(type_id, numbers)
+
+    return version, number, end
+
+
+def evaluate_number(data):
+    i = 0
+    last = False
+    number = ''
+    while not last:
+        number = '{}{}'.format(number, data[i + 1:i + 1 + 4])
+        last = data[i] == '0'
+        i += 5
+    number = int(number, 2)
+    end = i - 1
+    return number, end
+
+
+def evaluate_sub_packets_length(data):
+    versions = []
+    numbers = []
+
+    start = 0
+    last = False
+    while not last:
+        version, number, end = evaluate_data(data[start:])
+
+        versions.append(version)
+        numbers.append(number)
+
+        start += end
+        last = start == len(data)
+
+    return versions, numbers
+
+
+def evaluate_sub_packets_number(data, number_sub_packets):
+    versions = []
+    numbers = []
+
+    start = 0
+    end = 0
+    for i in range(number_sub_packets):
+        version, number, number_end = evaluate_data(data[start:])
+
+        versions.append(version)
+        numbers.append(number)
+
+        start += number_end
+        end += number_end
+
+    return versions, numbers, end
+
+
+def apply(type_id, numbers):
+    if type_id == 0:
+        return sum(numbers)
+    if type_id == 1:
+        return mul(numbers)
+    if type_id == 2:
+        return min(numbers)
+    if type_id == 3:
+        return max(numbers)
+    if type_id == 5:
+        return int(numbers[0] > numbers[1])
+    if type_id == 6:
+        return int(numbers[0] < numbers[1])
+    if type_id == 7:
+        return int(numbers[0] == numbers[1])
+
+    print("ERROR")
+    return 0
+
+
+def mul(list_):
+    res = 1
+    for i in list_:
+        res *= i
+    return res
+
+
+if __name__ == '__main__':
+    startTime = time.time()
+
+    main()
+
+    executionTime = (time.time() - startTime)
+    print('Execution time: ' + str(round(executionTime * 1000, 3)) + ' ms')
--- 2021/test.py
+++ 2021/test.py
@@ -3,28 +3,8 @@ from scanf import scanf
 from sortedcontainers import SortedKeyList
 
 
-class Node:
-    def __init__(self, risk, value=-1, visited=False, previous=None):
-        self.risk = risk
-        self.value = value
-        self.visited = visited
-        self.previous = previous
+a = True
+print(int(a))
+a = False
+print(int(a))
 
-    def __str__(self):
-        return "r: {}, v: {}, v: {}".format(self.risk, self.value, self.visited)
-
-
-def main():
-    a = Node(1)
-    b = Node(2)
-    c = Node(3)
-    todo = SortedKeyList([], lambda z: z.risk)
-    for e in [b, c, a]:
-        print([str(x) for x in todo])
-        todo.add(e)
-
-    print([str(x) for x in todo])
-
-
-if __name__ == '__main__':
-    main()
