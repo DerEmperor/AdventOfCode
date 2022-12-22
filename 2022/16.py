6c51386585e974681bbf21109c3e65753a594005
Tobias Kaiser <tobi.kaiser@dau.de>
2022-12-22 15:07:37 +0100

    2022 day 16, I give up

--- 2022/16.py
+++ 2022/16.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
+
+import functools
+import itertools
 import time
 import re
-from functools import cmp_to_key
-
-import numpy as np
+from collections import defaultdict
 
 
 class Valve:
@@ -20,7 +21,7 @@ class Valve:
         return hash(self.id)
 
     def __repr__(self):
-        return f'<V: {self.id}, r={self.flow_rate}, {set(v.id for v in self.dsts)}>'
+        return f'<V: {self.id}, r={self.flow_rate}, {set((v.id, d) for v, d in self.dsts)}>'
 
 
 def get_input(test):
@@ -40,58 +41,81 @@ def get_input(test):
     for valve in valves.values():
         new_dsts = set()
         for valve_id in valve.dsts:
-            new_dsts.add(valves[valve_id])
+            new_dsts.add((valves[valve_id], 1))
         valve.dsts = new_dsts
 
     return valves
 
 
-mem = dict()
+def floyd_warshall(valves):
+    distances = defaultdict(lambda: len(valves) + 1)
+    for valve in valves.values():
+        for valve2, dis in valve.dsts:
+            distances[valve.id, valve2.id] = dis
+            distances[valve2.id, valve.id] = dis
+        valve.dsts.clear()
+    for k, i, j in itertools.product([v.id for v in valves.values()], repeat=3):
+        distances[i, j] = min(distances[i, j], distances[i, k] + distances[k, j])
+
+    for (v_id, v2_id), dis in distances.items():
+        v = valves[v_id]
+        v2 = valves[v2_id]
+        if (v.flow_rate > 0 or v_id == 'AA') and v2.flow_rate > 0 and v_id != v2_id:
+            v.dsts.add((v2, dis))
+
+    # throw away valves with flow rate 0
+    for valve in list(valves.values()):
+        if valve.flow_rate == 0 and valve.id != 'AA':
+            del valves[valve.id]
 
 
-def helper(valve, score, time, open_valves):
-    if time <= 0:
-        return score
+def part1(valve, time_left):
+    if time_left <= 0 or valve.open:
+        return 0
 
-    state = (valve, score, time, tuple(open_valves))
-    if state in mem:
-        return mem[state]
+    score = 0
 
-    # don't open valve
-    for dst in valve.dsts:
+    for dst, dis in valve.dsts:
         # walk to new valve
-        new_score = helper(dst, score, time - 1, open_valves)
-        score = max(score, new_score)
+        valve.open = True
+        score = max(score, part1(dst, time_left - dis - (1 if valve.flow_rate > 0 else 0)) + (time_left - 1) * valve.flow_rate)
+        valve.open = False
 
-    # open valve
-    time -= 1  # time to open valve
-    if time > 0:
-        for dst in valve.dsts:
-            if dst.flow_rate > 0 and not valve.open:
-                # walk to new valve
-                valve.open = True
-                open_valves.add(valve.id)
-                new_score = helper(dst, score + time * dst.flow_rate, time - 1, open_valves)
-                open_valves.remove(valve.id)
-                valve.open = False
-                score = max(score, new_score)
-    mem[state] = score
+    return score
+
+
+@functools.cache
+def part2(start_valve, valve, open_valves, time_left, foo):
+    if time_left <= 0 or valve.id not in open_valves:
+        return 0
+
+    score = 0
+
+    for dst, dis in valve.dsts:
+        # walk to new valve
+        res = part2(start_valve, dst, open_valves - {valve.id}, time_left - dis - (1 if valve.flow_rate > 0 else 0), foo)
+        res += (time_left - 1) * valve.flow_rate
+        score = max(score, res)
+    if foo:
+        res = part2(start_valve, start_valve, open_valves | {'AA'}, 26, False)
+        score = max(score, res)
     return score
 
 
 def main(test):
     valves = get_input(test)
-    score = helper(valve=valves['AA'], score=0, time=30, open_valves = set())
-    print(score)
+    floyd_warshall(valves)
+    print(part1(valves['AA'], 30))
+    print(part2(valves['AA'], valves['AA'], frozenset(valves.keys()), 26, True))
 
 
 if __name__ == '__main__':
-    startTime = time.time()
+    start_time = time.time()
 
     print('Test')
     main(True)
     print('real')
-    # main(False)
+    main(False)
 
-    executionTime = (time.time() - startTime)
-    print('Execution time: ' + str(round(executionTime * 1000, 3)) + ' ms')
+    execution_time = (time.time() - start_time)
+    print('Execution time: ' + str(round(execution_time, 3)) + ' s')
