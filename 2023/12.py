5a4938682f9f976981996dfa697d2eb375255116
Tobias Kaiser <tobi.kaiser@fau.de>
2023-12-23 14:52:03 +0100

    day 12 takes too long

--- 2023/12.py
+++ 2023/12.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
 """
 Execution time:
-| part | 1      | 2               |
-|------|--------|-----------------|
-| test | 0.5 s  | 67 s            |
-| real | 1.85 s | 248 s = 4.1 min |
 sum: 5,5 min
 """
 from __future__ import annotations
 
+import functools
+import sys
 import time
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 from typing import List, Tuple
 
 
-def get_input(test: bool):
+def get_input(test: bool) -> List[Tuple[str, Tuple[int, ...]]]:
     data = []
     filename = f"inputs/{Path(__file__).stem}{'_test' if test else ''}.txt"
     with open(filename, 'r') as file:
         input_ = file.readlines()
         for line in input_:
             springs, checksum = line[:-1].split(' ')
-            checksum = [int(x) for x in checksum.split(',')]
-            data.append((list(springs), checksum))
+            checksum = tuple(int(x) for x in checksum.split(','))
+            data.append((springs, checksum))
     return data
 
 
 mem = {}
 
 
-def get_checksum(springs: List[str]) -> List[int]:
-    as_str = ''.join(springs)
-    res = mem.get(as_str, None)
-    if res is not None:
-        return res
+# @functools.cache
+def get_checksum(springs: str) -> Tuple[int, ...]:
+    if len(springs) < 20:
+        res = mem.get(springs, None)
+        if res is not None:
+            return res
 
     res = []
     i = 0
@@ -48,11 +47,18 @@ def get_checksum(springs: List[str]) -> List[int]:
                 i += 1
             res.append(cnt)
         i += 1
-    mem[as_str] = res
+    res = tuple(res)
+    if len(springs) < 20:
+        mem[springs] = res
     return res
 
 
-def helper(springs: List[str], checksum: List[int], i: int) -> int:
+def str_repl(string: str, i: int, c: str):
+    return string[:i] + c + string[i + 1:]
+
+
+@functools.cache
+def helper(springs: str, checksum: Tuple[int, ...], i: int) -> int:
     while i < len(springs) and springs[i] != '?':
         i += 1
 
@@ -67,54 +73,48 @@ def helper(springs: List[str], checksum: List[int], i: int) -> int:
         return 0
     if len(part_checksum) > 1 and part_checksum[:-1] != checksum[:len(part_checksum) - 1]:
         return 0
+    if len(part_checksum) > 0 and part_checksum[-1] > checksum[len(part_checksum) - 1]:
+        return 0
 
-    springs[i] = '#'
-    a = helper(springs, checksum, i + 1)
-    springs[i] = '.'
-    b = helper(springs, checksum, i + 1)
-    springs[i] = '?'
+    new_springs = str_repl(springs, i, '#')
+    a = helper(new_springs, checksum, i + 1)
+    new_springs = str_repl(springs, i, '.')
+    b = helper(new_springs, checksum, i + 1)
     return a + b
 
 
-def helper_caller(args: Tuple[List[str], List[int]]) -> int:
+def helper_caller(args: Tuple[str, Tuple[int, ...]]) -> int:
     springs, checksum = args
     return helper(springs, checksum, 0)
 
 
-def helper_caller2(args: Tuple[List[str], List[int]]) -> int:
+def helper_caller2(args: Tuple[str, Tuple[int, ...]]) -> int:
     springs, checksum = args
-    springs = springs * 5
+    springs = ((springs + '?') * 5)[:-1]
     checksum = checksum * 5
     return helper(springs, checksum, 0)
 
 
 def main(test: bool):
     data = get_input(test)
-
-    # single thread for debugging
-    """
-    sum1 = 0
-    for chunk in data:
-        springs, checksum = chunk
-        if checksum == [1, 6, 5]:
-            tmp = 5
-        res = helper_caller(chunk)
-        print(''.join(springs), checksum, res)
-        sum1 += res
-    print(sum1)
-    """
-
-    sum1 = 0
-    with ProcessPoolExecutor() as executor:
-        for possible_arrangements in executor.map(helper_caller, data):
-            sum1 += possible_arrangements
-    print(sum1)
-
-    sum2 = 0
-    with ProcessPoolExecutor() as executor:
-        for possible_arrangements in executor.map(helper_caller2, data):
-            sum2 += possible_arrangements
-    print(sum2)
+    single_thread = False
+    for part, fun in [(1, helper_caller), (2, helper_caller2)]:
+        # single thread
+        sum = 0
+        if single_thread:
+            for i, chunk in enumerate(data):
+                res = helper_caller2(chunk)
+                print('\rprogress:', i, '/', len(data), end='', flush=True)
+                sum += res
+        else:
+            i = 0
+            with ProcessPoolExecutor() as executor:
+                for possible_arrangements in executor.map(fun, data):
+                    sum += possible_arrangements
+                    i += 1
+                    print('\rprogress:', i, '/', len(data), end='', flush=True)
+                    sys.stdout.flush()
+        print('\rpart ', part, ':', sum)
 
 
 if __name__ == '__main__':
