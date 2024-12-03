1ad00a96b41959491aadaa2b2e21a40667ff5166
Tobias Kaiser <tobi.kaiser@dau.de>
2024-12-03 23:03:54 +0100

    day3 done

--- 2024/03.py
+++ 2024/03.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
+import re
 import time
+
+re_mul = re.compile(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
 
 
 def get_input(test):
-    data = []
-    filename = 'inputs/03_test.txt' if test else 'inputs/03.txt'
+    filename = 'inputs/03_test_1.txt' if test else 'inputs/03.txt'
     with open(filename, 'r') as file:
         input_ = file.readlines()
-        for line in input_:
-            data.append(line[:-1])
-    return data
+    return ''.join(input_)
 
 
 def main(test):
     data = get_input(test)
+    part1 = 0
+    part2 = 0
+    enabled = True
+    for match in re_mul.finditer(data):
+        if match.group(0) == 'do()':
+            enabled = True
+        elif match.group(0) == "don't()":
+            enabled = False
+        else:
+            a, b = [int(x) for x in match.groups()]
+            part1 += a * b
+            if enabled:
+                part2 += a * b
+
+    print('part1:', part1)
+    print('part2:', part2)
 
 
 if __name__ == '__main__':
