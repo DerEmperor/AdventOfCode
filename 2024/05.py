c7dbdcda4a56d9de5885fd920553d682f913eca4
Tobias Kaiser <tobi.kaiser@dau.de>
2024-12-05 17:28:19 +0100

    day 5 done

--- 2024/05.py
+++ 2024/05.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python
 import time
+from collections import defaultdict
 
 
 def get_input(test):
-    data = []
     filename = 'inputs/05_test.txt' if test else 'inputs/05.txt'
     with open(filename, 'r') as file:
         input_ = file.readlines()
-        for line in input_:
-            data.append(line[:-1])
-    return data
+
+    input_iter = iter(input_)
+
+    rules = defaultdict(list)
+    for line in input_iter:
+        if line == '\n':
+            break
+        a, b = [int(x) for x in line[:-1].split('|')]
+        rules[a].append(b)
+
+    pages = []
+    for line in input_iter:
+        pages.append([int(x) for x in line[:-1].split(',')])
+    return rules, pages
 
 
 def main(test):
-    data = get_input(test)
+    rules, pagess = get_input(test)
+
     part1 = 0
     part2 = 0
+
+    for pages in pagess:
+        valid = True
+        for i, page_a in enumerate(pages):
+            for page_b in pages[i + 1:]:
+                if page_b not in rules[page_a]:
+                    valid = False
+                    break
+            if not valid:
+                break
+
+        if valid:
+            part1 += pages[len(pages) // 2]
+
+        else:
+            corrected_pages = [pages[0]]
+            for page_ins in pages[1:]:
+                for i, page_b in enumerate(corrected_pages):
+                    if page_ins not in rules[page_b]:
+                        corrected_pages.insert(i, page_ins)
+                        break
+                if page_ins not in corrected_pages:
+                    corrected_pages.append(page_ins)
+            part2 += corrected_pages[len(corrected_pages) // 2]
+
     print('part1:', part1)
     print('part2:', part2)
 
---
+++ 2024/06.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import time
+
+
+def get_input(test):
+    data = []
+    filename = 'inputs/06_test.txt' if test else 'inputs/06.txt'
+    with open(filename, 'r') as file:
+        input_ = file.readlines()
+        for line in input_:
+            data.append(line[:-1])
+    return data
+
+
+def main(test):
+    data = get_input(test)
+    part1 = 0
+    part2 = 0
+    print('part1:', part1)
+    print('part2:', part2)
+
+
+if __name__ == '__main__':
+    startTime = time.time()
+
+    print('Test')
+    main(True)
+    print('real')
+    main(False)
+
+    executionTime = (time.time() - startTime)
+    print('Execution time: ' + str(round(executionTime * 1000, 3)) + ' ms')
