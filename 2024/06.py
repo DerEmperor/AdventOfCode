0366c8ac725efabb1ff512847227b9cb7c6ecd7a
Tobias Kaiser <tobias.kaiser@noris.de>
2024-12-06 13:47:46 +0100

    day 6 first try

--- 2024/06.py
+++ 2024/06.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python
 import time
+from itertools import cycle
+
+'''
++---> x
+|
+|
+v
+y
+'''
+
+directions = cycle([(0, -1), (1, 0), (0, 1), (-1, 0)])  # (x, y)
 
 
-def get_input(test):
-    data = []
+def get_input(test: bool):
+    obstructions = set()
+    start = None
     filename = 'inputs/06_test.txt' if test else 'inputs/06.txt'
     with open(filename, 'r') as file:
         input_ = file.readlines()
-        for line in input_:
-            data.append(line[:-1])
-    return data
+    dimensions = (len(input_[0]), len(input_))
+    for y, line in enumerate(input_):
+        for x, char in enumerate(line):
+            if char == '#':
+                obstructions.add((x, y))
+            elif char == '^':
+                assert start is None
+                start = (x, y)
+    assert start is not None
+    return obstructions, start, dimensions
 
 
-def main(test):
-    data = get_input(test)
-    part1 = 0
+def main(test: bool):
+    obstructions, start, dimensions = get_input(test)
+
     part2 = 0
-    print('part1:', part1)
+
+    direction = next(directions)
+    pos = start
+    visited = set()
+
+    while 0 <= pos[0] < dimensions[0] and 0 <= pos[1] < dimensions[1]:
+        visited.add(pos)
+        next_pos = pos[0] + direction[0], pos[1] + direction[1]
+        if next_pos in obstructions:
+            direction = next(directions)
+        else:
+            pos = next_pos
+
+    print('part1:', len(visited))
     print('part2:', part2)
 
 
