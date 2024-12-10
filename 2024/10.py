ab09c6c69f9f8acacca11697269f0d75b572171d
Tobias Kaiser <tobi.kaiser@dau.de>
2024-12-10 14:53:29 +0100

    day10 finished

--- 2024/10.py
+++ 2024/10.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
 import time
+from typing import List, Tuple
 
 
 def get_input(test: bool):
@@ -8,14 +11,38 @@ def get_input(test: bool):
     with open(filename, 'r') as file:
         input_ = file.readlines()
         for line in input_:
-            data.append(line[:-1])
+            data.append([int(x) for x in line[:-1]])
     return data
+
+
+def get_score(data: List[List[int]], x: int, y: int) -> List[Tuple[int, int]]:
+    if not (0 <= x < len(data[0]) and 0 <= y < len(data)):
+        # oob
+        return []
+    if data[y][x] >= 9:
+        return [(x, y)]
+
+    res = []
+    for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+        if 0 <= x + dx < len(data[0]) and 0 <= y + dy < len(data):
+            if data[y + dy][x + dx] == data[y][x] + 1:
+                res.extend(get_score(data, x + dx, y + dy))
+
+    return res
 
 
 def main(test: bool):
     data = get_input(test)
     part1 = 0
     part2 = 0
+
+    for y, line in enumerate(data):
+        for x, height in enumerate(line):
+            if height == 0:
+                res = get_score(data, x, y)
+                part1 += len(set(res))
+                part2 += len(res)
+
     print('part1:', part1)
     print('part2:', part2)
 
@@ -26,7 +53,7 @@ if __name__ == '__main__':
     print('Test')
     main(True)
     print('real')
-    #main(False)
+    main(False)
 
     executionTime = (time.time() - startTime)
     if executionTime > 1:
