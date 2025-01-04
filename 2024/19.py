fd8543dd095ad0a7a77c721524b09567ee7a540a
Tobias Kaiser <tobi.kaiser@fau.de>
2025-01-04 11:48:05 +0100

    day 19 done

--- 2024/19.py
+++ 2024/19.py
@@ -3,22 +3,49 @@
 from __future__ import annotations
 
 import time
+from typing import List, Tuple
 
 
 def get_input(test: bool):
-    data = []
+    designs = []
     filename = 'inputs/19_test.txt' if test else 'inputs/19.txt'
     with open(filename, 'r') as file:
         input_ = file.readlines()
-        for line in input_:
-            data.append(line[:-1])
-    return data
+        towels = input_[0][:-1].split(', ')
+        for line in input_[2:]:
+            designs.append(line[:-1])
+    return towels, designs
+
+
+def is_possible(design: str, towels: List[str]) -> int:
+    mem = {}
+
+    def is_possible_helper(i: int) -> int:
+        if i == len(design):
+            return 1
+        assert i < len(design)
+        if i in mem:
+            return mem[i]
+        res = 0
+        for towel in towels:
+            if design.startswith(towel, i):
+                res += is_possible_helper(i + len(towel))
+        mem[i] = res
+        return res
+
+    return is_possible_helper(0)
 
 
 def main(test: bool):
-    data = get_input(test)
+    towels, designs = get_input(test)
     part1 = 0
     part2 = 0
+    for i, design in enumerate(designs):
+        res = is_possible(design, towels)
+        if res > 0:
+            part1 += 1
+        part2 += res
+
     print('part1:', part1)
     print('part2:', part2)
 
@@ -29,7 +56,7 @@ if __name__ == '__main__':
     print('Test')
     main(True)
     print('real')
-    # main(False)
+    main(False)
 
     executionTime = (time.time() - startTime)
     if executionTime > 1:
